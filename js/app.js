396f54014267410bc4c0cc7a1e35f34cbfde3cb8
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-04-18 19:28:09 +0200

    Create win message and lose message modals

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 var allEnemies = [];
 var gemsCollected = 0;
-var gemCollected = false;
+var isGemCollected = false;
 var lives = 3;
 var score = 0;
-// isHurt variable for player touches an enemy .. it changes in engine.js file. line 99
-var isHurt = false;
+var isHurt = false; // isHurt variable for player touches an enemy .. it changes in engine.js file. line 99
 var startCharacter = 'images/char-boy.png';
+var countDownTimer;
+var isGameOver = false;
+document.getElementById('lives').innerHTML = lives;
+document.getElementById('gems').innerHTML = gemsCollected;
+var event;
 
 
+// General purpose function.
 function randomChoice(arr) {
 	return arr[Math.floor(Math.random() * arr.length)];
 }
-
-document.getElementById('lives').innerHTML = lives;
-document.getElementById('gems').innerHTML = gemsCollected;
 
 var Gem = function () {
 	this.sprite = 'images/' + randomChoice(['Gem-Blue', 'Gem-Orange', 'Gem-Green']) + '.png';
@@ -26,14 +28,13 @@ Gem.prototype.get = function () {
 };
 
 Gem.prototype.update = function () {
-	if (gemCollected === true) {
+	if (isGemCollected === true) {
 		this.gemX = randomChoice([0, 100, 200, 300, 400]);
 		this.gemY = randomChoice([59, 149, 230]);
-		this.get();
 		this.sprite = 'images/' + randomChoice(['Gem-Blue', 'Gem-Orange', 'Gem-Green']) + '.png';
-		gemCollected = false;
+		isGemCollected = false;
 	}
-
+	// to generate a new gem if gem position === player position.
 	if (gem.get()[0] === player.get()[0] &&
 		(player.get()[1] - gem.get()[1]) < 15 &&
 		(player.get()[1] - gem.get()[1]) > 0) {
@@ -49,11 +50,10 @@ var Enemy = function () {
 	this.sprite = 'images/' +
 		randomChoice(['enemy-bug-red', 'enemy-bug-blue', 'enemy-bug-green']) +
 		'.png';
-	this.speed = Math.floor(Math.random() * 100) + 150;
+	this.speed = Math.floor(Math.random() * 100) + 250;
 	this.enemyPosX = randomChoice([-100, -150, -40, -50, -15, -35]);
 	this.enemyPosY = randomChoice([59, 149, 230]);
 };
-
 
 Enemy.prototype.update = function (dt) {
 	if (this.enemyPosX < 490) {
@@ -63,7 +63,6 @@ Enemy.prototype.update = function (dt) {
 		this.enemyPosY = randomChoice([59, 149, 230]);
 	}
 };
-
 
 Enemy.prototype.get = function () {
 	return [this.enemyPosX, this.enemyPosY];
@@ -84,6 +83,7 @@ Player.prototype.render = function () {
 };
 
 Player.prototype.update = function () {
+	// case touch an enemy.
 	if (isHurt === true) {
 		this.playerY = 400;
 		this.playerX = randomChoice([0, 100, 200, 300, 400]);
@@ -92,6 +92,7 @@ Player.prototype.update = function () {
 		isHurt = false;
 		checkGameOver();
 	}
+	// case player raches top.
 	if (this.playerY === -15) {
 		score += 1;
 		document.getElementById('score').innerHTML = score;
@@ -110,42 +111,44 @@ Player.prototype.updateCharacter = function () {
 };
 
 Player.prototype.handleInput = function (key) {
-	switch (key) {
-		case 'up':
-			if (this.playerY > -15) this.playerY -= 83;
-			break;
-		case 'down':
-			if (this.playerY < 400) this.playerY += 83;
-			break;
-		case 'right':
-			if (this.playerX < 400) this.playerX += 100;
-			break;
-		case 'left':
-			if (this.playerX > 0) this.playerX -= 100;
-			break;
+	// if () {
+		switch (key) {
+			case 'up':
+				if (this.playerY > -15) this.playerY -= 83;
+				break;
+			case 'down':
+				if (this.playerY < 400) this.playerY += 83;
+				break;
+			case 'right':
+				if (this.playerX < 400) this.playerX += 100;
+				break;
+			case 'left':
+				if (this.playerX > 0) this.playerX -= 100;
+				break;
+		// }
+		checkCollectGems();
 	}
-	checkCollect();
 };
 
 
 var player = new Player;
-var gem = new Gem;
 var firstEnemy = new Enemy;
 var secondEnemy = new Enemy;
 var thirdEnemy = new Enemy;
+var gem = new Gem;
 
 allEnemies.push(firstEnemy);
 allEnemies.push(secondEnemy);
 allEnemies.push(thirdEnemy);
+// document.addEventListener('keyup', keyPress, false);
 
-
-function checkCollect() {
+function checkCollectGems() {
 	if (gem.get()[0] === player.get()[0] &&
 		(player.get()[1] - gem.get()[1]) < 15 &&
 		(player.get()[1] - gem.get()[1]) > 0) {
 		gemsCollected += 1;
 		document.getElementById('gems').innerHTML = gemsCollected;
-		gemCollected = true;
+		isGemCollected = true;
 	}
 }
 
@@ -155,7 +158,6 @@ function checkCollect() {
 
 /*count down timer 3:00 minutes */
 // change next line if you want to change time
-
 function countDown() {
 	var presentTime = document.getElementById('timer').innerHTML;
 	var timeArray = presentTime.split(/[:]+/);
@@ -168,7 +170,7 @@ function countDown() {
 
 	document.getElementById('timer').innerHTML =
 		minutes + ':' + seconds;
-	setTimeout(countDown, 1000);
+	countDownTimer = setTimeout(countDown, 1000);
 }
 
 function secondsCheck(sec) {
@@ -181,43 +183,64 @@ function secondsCheck(sec) {
 	return sec;
 }
 
-
 function checkGameOver() {
 	// No need to check timer.
 	//timer execute displayGameOver() when it's zero
 	if (lives === 0) {
 		displayGameOver();
+		isGameOver = true;
+		delete player;
+		resetBoard();
 	}
 }
 
 function displayGameOver() {
-	//dosomething
+	clearTimeout(countDownTimer);
+	openModal(loseModal);
 }
 
-function displayGameWinner(){
-	//dosomething
+function displayGameWinner() {
+	clearTimeout(countDownTimer);
+	openModal(winModal);
+	isGameOver = true;
+	resetBoard();
 }
+
+event = document.addEventListener('keyup', keyPress(), false);
 
 function startGame() {
-	lives = 3;
-	gemsCollected = 0;
-	score = 0;
-	player.updateCharacter();
-	closeModal();
-	document.getElementById('timer').innerHTML = '03' + ':' + '00';
+	isGameOver = false;
+	player = new Player;
+	closeModal(startingWindow);
+	closeModal(loseModal);
+	closeModal(winModal);
+	// resetBoard();
 	countDown();
-	document.addEventListener('keyup', function (e) {
+}
+
+function keyPress() {
+	return function (e) {
 		var allowedKeys = {
 			37: 'left',
 			38: 'up',
 			39: 'right',
 			40: 'down'
 		};
-
 		player.handleInput(allowedKeys[e.keyCode]);
-	});
+	};
 }
 
 function checkWin() {
 	if (score === 10) displayGameWinner();
+}
+
+function resetBoard() {
+	// clearTimeout(countDownTimer);
+	lives = 3;
+	document.getElementById('lives').innerHTML = lives;
+	gemsCollected = 0;
+	document.getElementById('gems').innerHTML = gemsCollected;
+	score = 0;
+	document.getElementById('score').innerHTML = score;
+	document.getElementById('timer').innerHTML = '03' + ':' + '00';
 }
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,17 @@
 // Get modal element
-var modal = document.getElementById('simpleModal');
-window.onload = openModal();
-// Get open modal button
-// var modalBtn = document.getElementById('modalBtn'); //deleteMe:
+var startingWindow = document.getElementById('simpleModal');
+// Function to open modal
+function openModal(modal) {
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(modal) {
+  modal.style.display = 'none';
+}
+// First thing happens .. window onload.
+window.onload = openModal(startingWindow);
+// Character select functionality
 var character = document.querySelectorAll('.character');
 
 character.forEach(char => {
@@ -16,21 +25,8 @@ function select(target) {
     target.target.classList = 'character selected';
     startCharacter = 'images/' + target.target.id + '.png';
     document.querySelector('.btn').style.display = 'inline';
-}
+} // end of Character select
 
-// Function to open modal
-function openModal() {
-  modal.style.display = 'block';
-}
+var winModal = document.getElementById('win-modal');
+const loseModal = document.getElementById('lose-modal');
 
-// Function to close modal
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-// Function to close modal if outside click
-function clickOutside(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
-}
